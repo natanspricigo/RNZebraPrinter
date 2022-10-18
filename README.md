8177a9d4f7b66e47371621438304bfd69e4da259
Natan <natanspricigo@gmail.com>
2022-10-18 18:20:37 -0300

    chore: linking

--- README.md
+++ README.md
@@ -6,24 +6,3 @@ Imprssão via bluetooth zebra
 npm install rn-zebra-printer-bluetooth
 ```
 
-## Usage
-
-```js
-import { multiply } from "rn-zebra-printer-bluetooth";
-
-// ...
-
-const result = await multiply(3, 7);
-```
-
-## Contributing
-
-See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
-
-## License
-
-MIT
-
----
-
-Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,6 @@
-import { NativeModules, Platform } from 'react-native';
 
-const LINKING_ERROR =
-  `The package 'rn-zebra-printer-bluetooth' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo managed workflow\n';
+import { NativeModules } from 'react-native';
 
-const RnZebraPrinterBluetooth = NativeModules.RnZebraPrinterBluetooth  ? NativeModules.RnZebraPrinterBluetooth  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
+const { RnZebraPrinterBluetooth } = NativeModules;
 
-export function multiply(a: number, b: number): Promise<number> {
-  return RnZebraPrinterBluetooth.multiply(a, b);
-}
+export default RnZebraPrinterBluetooth;
